8259fa23df73491342054f57985a7a3e8c05e92d
f.sousa <ng.dev.sousa@outlook.com>
2021-03-10 21:08:54 -0300

    Add tests

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,23 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "it should return the length of the provided list" do
+      list = [1, 2, 3]
+
+      result = ListLength.call(list)
+      expected_result = 3
+
+      assert result == expected_result
+    end
+
+    test "it should return zero if an empty list is provided" do
+      list = []
+
+      result = ListLength.call(list)
+      expected_result = 0
+
+      assert result == expected_result
+    end
   end
 end
